00dd9f75f0869755166bb20df26b9c8f9341be4e
stelle <estellejordan1999@gmail.com>
2022-04-16 12:44:03 -0700

    more images and cycle through them

--- main.js
+++ main.js
@@ -3,20 +3,30 @@ console.log("foo")
 Vue.createApp({
     data() {
       return {
-        titles: ['The Cat', 'The Adventurer', 'The Test Pilot', 'The Flea Exterminator', 'The Cheesepuff Lover', 'The Greenies-Fiender'],
+        title_image: [
+          ['The Cat', "/img/microbe_wanted.jpg"],
+          ['The Adventurer', "/img/outdoorkitty.jpg"],
+          ['The Adventurer', "/img/saki_biker.jpg"],
+          ['The Test Pilot', "/img/saki_boxtime.png"],
+          ['The Elf', "/img/micheal_microbe.jpg"],
+          ['The Cheesepuff Lover', "/img/microbe_cheesepuff2.jpg"],
+          ['The Stylish', "/img/sweaterkitty.jpg"],
+          ['The Kitten', '/img/kitten.jpg'],
+          ['The Plumber', '/img/saki_toilet.jpg']
+      ],
         index: 0
       }
     },
     methods: {
         increment_index_forever(){
             this.index++
-            if (this.index >= this.titles.length){
+            if (this.index >= this.title_image.length){
                 this.index = 0
             }
         }
     },
     mounted(){
-        setInterval(this.increment_index_forever, 1200)
+        setInterval(this.increment_index_forever, 2000)
     }
 
   }).mount('#app')
