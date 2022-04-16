f3f087cc53ea153ca5824b426780015549dd3e83
Conrad Menchine <conrad.menchine@gmail.com>
2022-04-16 11:53:24 -0700

    added titles

--- main.js
+++ main.js
@@ -3,7 +3,20 @@ console.log("foo")
 Vue.createApp({
     data() {
       return {
-        title: 'The Cat'
+        titles: ['The Cat', 'The Adventurer', 'The Test Pilot', 'The Flea Exterminator', 'The Cheesepuff Lover', 'The Greenies-Fiender'],
+        index: 0
       }
+    },
+    methods: {
+        increment_index_forever(){
+            this.index++
+            if (this.index >= this.titles.length){
+                this.index = 0
+            }
+        }
+    },
+    mounted(){
+        setInterval(this.increment_index_forever, 1200)
     }
+
   }).mount('#app')
