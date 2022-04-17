7f22d7202e860e2c42c3a0f9d4056309260d63db
Conrad Menchine <conrad.menchine@gmail.com>
2022-04-16 20:13:40 -0700

    removed hidden page

--- main.js
+++ main.js
@@ -13,8 +13,19 @@ Vue.createApp({
           ['The Stylish', "/img/sweaterkitty.jpg"],
           ['The Kitten', '/img/kitten.jpg'],
           ['The Plumber', '/img/saki_toilet.jpg']
-      ],
-        index: 0
+        ],
+        cat_names: [
+            "Saki",
+            "Microbe",
+            "Micro-Chat",
+            "Dingle",
+            "Orla",
+            "Who Names A Cat Orla??",
+            "Kitty"
+        ],
+        alt_name: "Microbe",
+        index: 0,
+        name_index: 0,
       }
     },
     methods: {
@@ -24,7 +35,18 @@ Vue.createApp({
             if (this.index >= this.title_image.length){
                 this.index = 0
             }
+        },
+        change_cat_name(){
+            this.name_index++;
+            if (this.name_index >= this.cat_names.length){
+                this.name_index = 0
+            }
+            this.alt_name = this.cat_names[this.name_index + 1]
+            if (this.name_index +1 >= this.cat_names.length)
+            this.alt_name = this.cat_names[0]
+
         }
+
     },
     mounted(){
         setInterval(this.increment_index_forever, 2000)
