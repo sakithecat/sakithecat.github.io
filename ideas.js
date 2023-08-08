dabbca85d4f12ff4ba094e47d745a338966a878e
Conrad Menchine <conrad@menchine.com>
2023-08-07 22:36:35 -0700

    added better scaling and more ideas

--- ideas.js
+++ ideas.js
@@ -11,20 +11,27 @@ function createHiPPICanvas(width, height) {
 
     return canvas;
 }
-var canvas = createHiPPICanvas(700,700)
-context = canvas.getContext('2d');
 
-var SIZE=700
+var SIZE=500
+var RADIUS = SIZE/10
+var canvas = createHiPPICanvas(SIZE,SIZE)
+context = canvas.getContext('2d');
 canvas.width = SIZE
 canvas.height = SIZE
 document.body.appendChild(canvas);
-console.log("test")
 
 context.strokeStyle = "white";
 context.fillStyle = "white";
 
 context.textAlign = 'center';
 
+var button = document.createElement("button");
+button.innerText = "Have Idea"
+button.addEventListener('click', newIdea)
+button.classList.add("butt")
+button.classList.add("shadow")
+var textbox = document.createElement("h2");
+textbox.innerHTML = "<br>"
 function drawLine(ctx,x1,y1,x2,y2){
     ctx.beginPath()
     ctx.moveTo(x1,y1)
@@ -45,7 +52,8 @@ function drawIdea(ctx, edges, n_brain_cells=4){
     // ctx.lineWidth = 10;
 
     function indexToPos(index){
-        return [SIZE/4 + Boolean(index & 1) * SIZE/2, SIZE/4 + Boolean(index & 2) * SIZE/2]
+        padded_radius = RADIUS * 1.1
+        return [padded_radius + Boolean(index & 1) * (SIZE- 2*padded_radius), padded_radius + Boolean(index & 2) * (SIZE- 2*padded_radius),]
     }
     //draw circles
     for (var i = 0; i < n_brain_cells; i++){
@@ -65,11 +73,9 @@ function drawIdea(ctx, edges, n_brain_cells=4){
         [2,3]
     ]
     vertices.forEach((v, i)=>{
-        console.log(i, edges, (1 << i))
         if (edges & (1<<i)){
             start = indexToPos(v[0])
             end =   indexToPos(v[1])
-            console.log(start, end)
             drawLine(ctx, start[0], start[1], end[0], end[1])
         }
     })
@@ -81,15 +87,24 @@ known_saki_ideas = [
     "make a program that simulates saki's ideas",
     "identify and eat a bug",
     "sploot",
-    ["4x4 mode", 0b001100],
     "decide that a bug is actually a lint",
     "point eyeballs in different directions",
     "Steal Ino's food",
-    "Bite or Lick",
+    "Lick mode! (or Bite mode?)",
     "Create an alphabet based on all possible ideas",
     "Spend a while making cool shapes out of lines",
     "Make an instagram post",
-    ["Try very hard to have two ideas at the same time", 0b10010]
+    "Pry blanket off of Estelle if she sleeps in past 7:00 AM",
+    "Lament about height of tall furniture",
+    "Tear open leftover burrito. (Don't worry about the tin foil)",
+    "Squeeze head out of window crack (and get stuck)",
+    "Become vacuum cleaner (deposit lint on next person to pick saki up)",
+    "Eat trash can snack",
+    ["Steal fish food", 0b1110],
+    ["Jump inside cardboard box", 0b110010],
+    ["4x4 mode", 0b001100],
+    ["Try very hard to have two ideas at the same time", 0b10010],
+    ["The most advanded idea possible ... (It's unknown if Saki has yet acheived this thought)", 0b111111]
     
 ]
 
@@ -104,17 +119,15 @@ function load_illegal_ideas(){
 
 }
 load_illegal_ideas()
-console.log(illegal_ideas)
 function random_idea(){
     var idea_index = Math.floor(Math.random()*known_saki_ideas.length)
     var idea = known_saki_ideas[idea_index];
 
     var idea_shape = 0
-    while (idea_shape in illegal_ideas){
+    while (illegal_ideas.includes(idea_shape)){
         idea_shape = ~~(Math.random()*(1<<6))
     }
     known_saki_ideas.splice(idea_index,1)
-    console.log(known_saki_ideas, idea)
     if (idea == undefined){
         idea = "Uh-oh. Saki is out of ideas."
         idea_shape = 0
@@ -137,47 +150,44 @@ function newIdea(){
     var idea = random_idea()
     
     drawIdea(context,idea.shape )
-    drawText(context, idea.text, SIZE/2, SIZE*.9 )
+    textbox.innerHTML = idea.text
 }
 function start(){
     clear()
     drawIdea(context,0,4)
     var br =  document.createElement("br");
     document.body.appendChild(br)
-    var button = document.createElement("button");
-    button.innerText = "Have Idea"
-    button.addEventListener('click', newIdea)
+
+    document.body.appendChild(textbox)
     document.body.appendChild(button)
-    button.classList.add("butt")
-    button.classList.add("shadow")
 }
 function intro(){
     // document.getElementById("idea_button").style.display = 'none';
     clear()
     setTimeout(()=>{
-        drawText(context, "Saki's", SIZE/2, SIZE*.2, 130)
+        drawText(context, "Saki's", SIZE/2, SIZE*.2, SIZE/5)
         setTimeout(()=>{
-            drawText(context, "Idea", SIZE/2, SIZE*.5, 130)
+            drawText(context, "Idea", SIZE/2, SIZE*.5, SIZE/5)
             setTimeout(()=>{
-                drawText(context, "Generator.", SIZE/2, SIZE*.8, 130)
+                drawText(context, "Generator", SIZE/2, SIZE*.8, SIZE/5.5)
                 setTimeout(()=>{
                     clear()
-                    drawText(context, "Saki only has 4 brain cells...", SIZE/2, SIZE/2, 30)
+                    drawText(context, "Saki only has 4 brain cells...", SIZE/2, SIZE/2, SIZE/20)
                     setTimeout(()=>{
                         clear()
-                        drawText(context, "Saki only has 4 brain cells...", SIZE/2, SIZE/2, 30)
+                        drawText(context, "Saki only has 4 brain cells...", SIZE/2, SIZE/2, SIZE/20)
                         drawIdea(context,0,1)
                         setTimeout(()=>{
                             clear()
-                            drawText(context, "Saki only has 4 brain cells...", SIZE/2, SIZE/2, 30)
+                            drawText(context, "Saki only has 4 brain cells...", SIZE/2, SIZE/2, SIZE/20)
                             drawIdea(context,0,2)
                             setTimeout(()=>{
                                 clear()
-                                drawText(context, "Saki only has 4 brain cells...", SIZE/2, SIZE/2, 30)
+                                drawText(context, "Saki only has 4 brain cells...", SIZE/2, SIZE/2, SIZE/20)
                                 drawIdea(context,0,3)
                                 setTimeout(()=>{
                                     clear()
-                                    drawText(context, "Saki only has 4 brain cells...", SIZE/2, SIZE/2, 30)
+                                    drawText(context, "Saki only has 4 brain cells...", SIZE/2, SIZE/2, SIZE/20)
                                     drawIdea(context,0,4)
                                     setTimeout(()=>{
                                         start()
